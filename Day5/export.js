c3f2fbcf11dc39a92d74f57f675578fcedf04f66
Nwaylaezaw6 <nwaylaezaw1762000@gmail.com>
2025-05-14 13:51:07 +0630

    import/export

---
+++ Day5/export.js
@@ -0,0 +1,7 @@
+export  function add(n1,n2){
+    console.log(n1+n2);
+}
+
+export  function mul(n1,n2){
+    console.log(n1*n2);
+}
---
+++ Day5/import.js
@@ -0,0 +1,9 @@
+//import add from "./export.js";
+//add(2,3);
+
+//import * as math from "./export.js";
+//math.add(3,4);
+
+import {add,mul} from "./export.js";
+add(2,3);
+mul(2,3);
